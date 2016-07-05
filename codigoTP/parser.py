649c2db5c3990ec0f16fb9740716448db7be67a0
Javier Nicolas Minces Muller <jminces@ws3.labo2>
2016-07-05 18:24:39 -0300

    Vamo que ganamo

--- codigoTP/parser.py
+++ codigoTP/parser.py
@@ -4,7 +4,7 @@ import lex
 import yacc
 lexer = lex.lex(module=lexer_rules)
 parser = yacc.yacc(module=parser_rules)
-text = "a = \"tu vieja\"; a++;"# if (true OR false) {a = 100;a = 200; holu = 12;} \n holu[14] = 14;\n print(\"Mundo!\"); poneenter = 12; return v=9; 1 == 2;"
+text = "a = \"a\"; a++;"# if (true OR false) {a = 100;a = 200; holu = 12;} \n holu[14] = 14;\n print(\"Mundo!\"); poneenter = 12; return v=9; 1 == 2;"
 ast = parser.parse(text, lexer)
 
 print ast
--- codigoTP/parser_rules.py
+++ codigoTP/parser_rules.py
@@ -7,268 +7,272 @@ types=dict()
 class SemanticException(Exception) :
 	pass
 
-def p_inicial(subexpressions):
+def p_inicial(subexps):
     'ss : s'
-    subexpressions[0] = subexpressions[1]
+    subexps[0] = subexps[1]
 
-def p_inicial2(subexpressions):
+def p_inicial2(subexps):
     'ss : s NEWLINE ss'
-    subexpressions[0] = subexpressions[1] + subexpressions[2] + subexpressions[3] 
+    subexps[0] = subexps[1] + subexps[2] + subexps[3] 
 
-def p_inicial3(subexpressions):
+def p_inicial3(subexps):
     'ss : s ss'
-    subexpressions[0] = subexpressions[1] + '\n' + subexpressions[2] 
+    subexps[0] = subexps[1] + '\n' + subexps[2] 
 
-def p_s_if(subexpressions):
+def p_s_if(subexps):
     's : if'
-    subexpressions[0] = subexpressions[1]
+    subexps[0] = subexps[1]
     
-def p_s_while(subexpressions):
+def p_s_while(subexps):
     's : w'
-    subexpressions[0] = subexpressions[1]    
+    subexps[0] = subexps[1]    
 
-def p_s_do(subexpressions):
+def p_s_do(subexps):
     's : d'
-    subexpressions[0] = subexpressions[1]
+    subexps[0] = subexps[1]
 
-def p_s_for(subexpressions):
+def p_s_for(subexps):
     's : for'
-    subexpressions[0] = subexpressions[1]
+    subexps[0] = subexps[1]
     
-def p_s_asig(subexpressions):
+def p_s_asig(subexps):
     's : a PUNTOCOMA'
-    subexpressions[0] = subexpressions[1] + subexpressions[2] 
+    subexps[0] = subexps[1] + subexps[2] 
     
-def p_s_o(subexpressions):
+def p_s_o(subexps):
     's : o PUNTOCOMA'
-    subexpressions[0] = subexpressions[1] + subexpressions[2] 
+    subexps[0] = subexps[1] + subexps[2] 
 
-def p_s_r(subexpressions):
+def p_s_r(subexps):
     's : RETURN r PUNTOCOMA'
-    subexpressions[0] = subexpressions[1] + ' ' + subexpressions[2] + subexpressions[3]
+    subexps[0] = subexps[1] + ' ' + subexps[2] + subexps[3]
 
-def p_s_coment(subexpressions):
+def p_s_coment(subexps):
     's : COMENTARIO'
-    subexpressions[0] = subexpressions[1]
+    subexps[0] = subexps[1]
 
-def p_r_v(subexpressions):
+def p_r_v(subexps):
     'r : v'
-    subexpressions[0] = subexpressions[1]["value"]
+    subexps[0] = subexps[1]["value"]
 
-def p_r_a(subexpressions):
+def p_r_a(subexps):
     'r : a'
-    subexpressions[0] = subexpressions[1]
+    subexps[0] = subexps[1]
 
-def p_r_o(subexpressions):
+def p_r_o(subexps):
     'r : o'
-    subexpressions[0] = subexpressions[1]
+    subexps[0] = subexps[1]
 
-def p_if(subexpressions):
+def p_if(subexps):
     'if : IF LPAREN v RPAREN bq'
-    subexpressions[0] = subexpressions[1] + ' ' + subexpressions[2] + subexpressions[3] + subexpressions[4] + subexpressions[5]
+    subexps[0] = subexps[1] + ' ' + subexps[2] + subexps[3] + subexps[4] + subexps[5]
     
-def p_if_else(subexpressions):
+def p_if_else(subexps):
     'if : IF LPAREN v RPAREN bq ELSE bq'
-    subexpressions[0] = subexpressions[1] + ' ' + subexpressions[2] + subexpressions[3] + subexpressions[4] + subexpressions[5] + '\n' + subexpressions[6] + subexpressions[7]
+    subexps[0] = subexps[1] + ' ' + subexps[2] + subexps[3] + subexps[4] + subexps[5] + '\n' + subexps[6] + subexps[7]
 
-def p_while(subexpressions):
+def p_while(subexps):
 	'w : WHILE LPAREN v RPAREN bq'
-	subexpressions[0] = subexpressions[1] + subexpressions[2] + subexpressions[3] + subexpressions[4] + '\n\t' + subexpressions[5]
+	subexps[0] = subexps[1] + subexps[2] + subexps[3] + subexps[4] + '\n\t' + subexps[5]
 
-def p_do_while(subexpressions):
+def p_do_while(subexps):
 	'd : DO bq WHILE LPAREN v RPAREN PUNTOCOMA'
-	subexpressions[0] = subexpressions[1] + subexpressions[2] + subexpressions[3] + ' ' + subexpressions[4] + subexpressions[5] + subexpressions[6] + subexpressions[7]
+	subexps[0] = subexps[1] + subexps[2] + subexps[3] + ' ' + subexps[4] + subexps[5] + subexps[6] + subexps[7]
 
-def p_for1(subexpressions):
+def p_for1(subexps):
 	'for : FOR LPAREN v RPAREN bq'
-	subexpressions[0] = subexpressions[1] + ' ' + subexpressions[2] + subexpressions[3] + subexpressions[4] + '\n\t' + subexpressions[5]
+	subexps[0] = subexps[1] + ' ' + subexps[2] + subexps[3] + subexps[4] + '\n\t' + subexps[5]
 
-def p_for2(subexpressions):
+def p_for2(subexps):
 	'for : FOR LPAREN a PUNTOCOMA v RPAREN bq'
-	subexpressions[0] = subexpressions[1] + ' ' + subexpressions[2] + subexpressions[3] + subexpressions[4] + ' ' + subexpressions[5] + subexpressions[6] + '\n\t' + subexpressions[7]
+	subexps[0] = subexps[1] + ' ' + subexps[2] + subexps[3] + subexps[4] + ' ' + subexps[5] + subexps[6] + '\n\t' + subexps[7]
 
-def p_for3(subexpressions):
+def p_for3(subexps):
 	'for : FOR LPAREN a PUNTOCOMA v PUNTOCOMA o RPAREN bq'
-	subexpressions[0] = subexpressions[1] + ' ' + subexpressions[2] + subexpressions[3] + subexpressions[4] + ' ' + subexpressions[5] + subexpressions[6] + ' ' + subexpressions[7] + subexpressions[8] + '\n\t' + subexpressions[9]
+	subexps[0] = subexps[1] + ' ' + subexps[2] + subexps[3] + subexps[4] + ' ' + subexps[5] + subexps[6] + ' ' + subexps[7] + subexps[8] + '\n\t' + subexps[9]
 
-def p_b_true(subexpressions):
+def p_b_true(subexps):
     'f : TRUE'
-    subexpressions[0] = subexpressions[1]
+    subexps[0] = subexps[1]
 
-def p_b_false(subexpressions):
+def p_b_false(subexps):
     'f : FALSE'
-    subexpressions[0] = subexpressions[1]
+    subexps[0] = subexps[1]
 
-def p_b_not(subexpressions):
+def p_b_not(subexps):
     'v : NOT f'
-    subexpressions[0] = subexpressions[1] + subexpressions[2]
+    subexps[0] = subexps[1] + subexps[2]
     
-def p_b_or(subexpressions):
+def p_b_or(subexps):
     'v : v OR f'
-    subexpressions[0] = subexpressions[1] + ' ' + subexpressions[2] + ' ' + subexpressions[3]
+    subexps[0] = subexps[1] + ' ' + subexps[2] + ' ' + subexps[3]
     
-def p_b_and(subexpressions):
+def p_b_and(subexps):
     'v : v AND f'
-    subexpressions[0] = subexpressions[1] + ' ' + subexpressions[2] + ' ' + subexpressions[3]
+    subexps[0] = subexps[1] + ' ' + subexps[2] + ' ' + subexps[3]
 
-def p_b_igualigual(subexpressions):
+def p_b_igualigual(subexps):
     'v : v IGUAL IGUAL f'
-    subexpressions[0] = subexpressions[1]["value"] + ' ' + subexpressions[2] + subexpressions[3] + ' ' + subexpressions[4]["value"]
+    subexps[0] = subexps[1]["value"] + ' ' + subexps[2] + subexps[3] + ' ' + subexps[4]["value"]
 
-def p_b_desigualdad(subexpressions):
+def p_b_desigualdad(subexps):
     'v : v DESIGUALDAD f'
-    subexpressions[0] = subexpressions[1]["value"] + ' ' + subexpressions[2] + ' ' + subexpressions[3]["value"]
+    subexps[0] = subexps[1]["value"] + ' ' + subexps[2] + ' ' + subexps[3]["value"]
     
-def p_b_mayor(subexpressions):
+def p_b_mayor(subexps):
     'v : v MAYOR f'
-    subexpressions[0] = subexpressions[1]["value"] + ' ' + subexpressions[2] + ' ' + subexpressions[3]["value"]
+    subexps[0] = subexps[1]["value"] + ' ' + subexps[2] + ' ' + subexps[3]["value"]
 
-def p_b_menor(subexpressions):
+def p_b_menor(subexps):
     'v : v MENOR f'
-    subexpressions[0] = subexpressions[1]["value"] + ' ' + subexpressions[2] + ' ' + subexpressions[3]["value"]
+    subexps[0] = subexps[1]["value"] + ' ' + subexps[2] + ' ' + subexps[3]["value"]
 
-def p_b_colineales(subexpressions):
+def p_b_colineales(subexps):
     'v : COLINEALES LPAREN v COMA v RPAREN'
-    subexpressions[0] = subexpressions[1] + subexpressions[2] + subexpressions[3]["value"] + subexpressions[4] + ' ' + subexpressions[5]["value"] + subexpressions[6]    
+    subexps[0] = subexps[1] + subexps[2] + subexps[3]["value"] + subexps[4] + ' ' + subexps[5]["value"] + subexps[6]    
     
-def p_v_parentesis(subexpressions):
+def p_v_parentesis(subexps):
     'f : LPAREN v RPAREN'
-    subexpressions[0] = subexpressions[1] + subexpressions[2]["value"] + subexpressions[3]
+    subexps[0] = subexps[1] + subexps[2]["value"] + subexps[3]
 
-def p_f_v(subexpressions):
+def p_f_v(subexps):
     'v : f'
-    subexpressions[0] = subexpressions[1]
+    subexps[0] = subexps[1]
 
-def p_v_resta(subexpressions):
+def p_v_resta(subexps):
     'v : v MENOS f'
-    subexpressions[0] = subexpressions[1]["value"] + subexpressions[2] + subexpressions[3]["value"]
+    subexps[0] = subexps[1]["value"] + subexps[2] + subexps[3]["value"]
 
-def p_v_suma(subexpressions):
+def p_v_suma(subexps):
     'v : v SUMA f'
-    subexpressions[0] = subexpressions[1]["value"] + subexpressions[2] + subexpressions[3]["value"]
+    subexps[0] = subexps[1]["value"] + subexps[2] + subexps[3]["value"]
 
-def p_v_por(subexpressions):
+def p_v_por(subexps):
     'v : v POR f'
-    subexpressions[0] = subexpressions[1]["value"] + subexpressions[2] + subexpressions[3]["value"]
+    subexps[0] = subexps[1]["value"] + subexps[2] + subexps[3]["value"]
     
-def p_v_div(subexpressions):
+def p_v_div(subexps):
     'v : v DIV f'
-    subexpressions[0] = subexpressions[1]["value"] + subexpressions[2] + subexpressions[3]["value"]
+    subexps[0] = subexps[1]["value"] + subexps[2] + subexps[3]["value"]
     
-def p_v_potencia(subexpressions):
+def p_v_potencia(subexps):
     'v : v POTENCIA f'
-    subexpressions[0] = subexpressions[1]["value"] + subexpressions[2] + subexpressions[3]["value"]
+    subexps[0] = subexps[1]["value"] + subexps[2] + subexps[3]["value"]
     
-def p_v_mod(subexpressions):
+def p_v_mod(subexps):
     'v : v PORCENTAJE f'
-    subexpressions[0] = subexpressions[1]["value"] + subexpressions[2] + subexpressions[3]["value"]
+    subexps[0] = subexps[1]["value"] + subexps[2] + subexps[3]["value"]
 
-def p_v_pregunta(subexpressions):
+def p_v_pregunta(subexps):
     'v : v INTERROGACION v DOSPUNTOS f'
-    subexpressions[0] = subexpressions[1] + subexpressions[2] + ' ' + subexpressions[3] + ' ' + subexpressions[4] + ' ' + subexpressions[5]
+    subexps[0] = subexps[1] + subexps[2] + ' ' + subexps[3] + ' ' + subexps[4] + ' ' + subexps[5]
      
-def p_v_num(subexpressions):
+def p_v_num(subexps):
     'f : NUMBER'
-    red = {"value": str(subexpressions[1]["value"]), "type": subexpressions[1]["type"]}
-    subexpressions[0] = red
-    
-def p_v_var(subexpressions):
-    'asig : VARIABLE'
+    red = {"value": str(subexps[1]["value"]), "type": subexps[1]["type"]}
+    subexps[0] = red
+
+def p_v_var(subexps):
+    'v : VARIABLE'
     '''asegurarse que el tipo de la variable siga siendo el mismo'''
-    subexpressions[0] = subexpressions[1]
+    subexps[0] = subexps[1]
     
-def p_v_mEvvb(subexpressions):
+def p_v_mEvvb(subexps):
     'v : MULTIPLICACIONESCALAR LPAREN v COMA v COMA v RPAREN'
-    subexpressions[0] = subexpressions[1] + subexpressions[2] + subexpressions[3] + subexpressions[4] + subexpressions[5] + subexpressions[6] + subexpressions[7] + subexpressions[8]
+    subexps[0] = subexps[1] + subexps[2] + subexps[3] + subexps[4] + subexps[5] + subexps[6] + subexps[7] + subexps[8]
         
-def p_v_mEvv(subexpressions):
+def p_v_mEvv(subexps):
     'v : MULTIPLICACIONESCALAR LPAREN v COMA v RPAREN'
-    subexpressions[0] = subexpressions[1] + subexpressions[2] + subexpressions[3] + subexpressions[4] + subexpressions[5] + subexpressions[6]
+    subexps[0] = subexps[1] + subexps[2] + subexps[3] + subexps[4] + subexps[5] + subexps[6]
 
-def p_v_cap(subexpressions):
+def p_v_cap(subexps):
     'v : CAPITALIZAR LPAREN v RPAREN'
-    if (types[subexpressions[1]] not in ["CADENA"]): raise SemanticException("Incompatible type")
-    subexpressions[0] = subexpressions[1] + subexpressions[2] + subexpressions[3] + subexpressions[4]
+    if (types[subexps[1]] not in ["CADENA"]): raise SemanticException("Incompatible type")
+    subexps[0] = subexps[1] + subexps[2] + subexps[3] + subexps[4]
 
-def p_v_legth(subexpressions):
+def p_v_legth(subexps):
     'v : LENGTH LPAREN v RPAREN'
-    subexpressions[0] = subexpressions[1] + subexpressions[2] + subexpressions[3] + subexpressions[4]
+    if (types[subexps[3]] not in ["int", "float"]): raise SemanticException("Incompatible type")
 
-def p_v_cadena(subexpressions):
+    subexps[0] = subexps[1] + subexps[2] + subexps[3] + subexps[4]
+
+def p_v_cadena(subexps):
     'v : CADENA'
-    subexpressions[0] = subexpressions[1]
+    subexps[0] = subexps[1]
 
-def p_v_asignable(subexpressions):
-    'asig : VARIABLE'
-    subexpressions[0] = subexpressions[1]
+def p_v_asignable(subexps):
+    'v : VARIABLE'
+    subexps[0] = subexps[1]
 
-def p_v_asignable(subexpressions):
-    'asig : asig CORCHETEA v CORCHETEC'
-    if (subexpressions[3]["type"] not in ["int", "float"]): raise SemanticException("Incompatible type")
-    subexpressions[0] = subexpressions[1]
+def p_v_asignable(subexps):
+    'v : v CORCHETEA v CORCHETEC'
+    if (subexps[3]["type"] not in ["int", "float"]): raise SemanticException("Incompatible type")
+    types[subexps[0]] = subexps[1]["type"]
+    subexps[0] = subexps[1]
 
-def p_v_asignable2(subexpressions):
-    'asig : asig PUNTO VARIABLE'
-    subexpressions[0] = subexpressions[1]
+def p_v_asignable2(subexps):
+    'v : v PUNTO VARIABLE'
+    types[subexps[0]] = subexps[3]["type"]
+    subexps[0] = subexps[1] + subexps[2] + subexps[3]
     
-def p_a_var(subexpressions):
-    'a : asig IGUAL v'
-    types[subexpressions[1]] = subexpressions[3]["type"]
-    subexpressions[0] = subexpressions[1] + ' ' + subexpressions[2] + ' ' + subexpressions[3]["value"]
+def p_a_var(subexps):
+    'a : v IGUAL v'
+    types[subexps[1]] = subexps[3]["type"]
+    subexps[0] = subexps[1] + ' ' + subexps[2] + ' ' + subexps[3]["value"]
 
-def p_o_masigual(subexpressions):
-    'o : asig SUMA IGUAL v'
-    if (types[subexpressions[1]] not in ["int", "float"]): raise SemanticException("Incompatible type")
-    if (types[subexpressions[4]] not in ["int", "float"]): raise SemanticException("Incompatible type")
-    subexpressions[0] = subexpressions[1] + ' ' + subexpressions[2] + subexpressions[3] + ' ' + subexpressions[4]["value"]
+def p_o_masigual(subexps):
+    'o : v SUMA IGUAL v'
+    if (types[subexps[1]] not in ["int", "float"]): raise SemanticException("Incompatible type")
+    if (types[subexps[4]] not in ["int", "float"]): raise SemanticException("Incompatible type")
+    subexps[0] = subexps[1] + ' ' + subexps[2] + subexps[3] + ' ' + subexps[4]["value"]
     
-def p_o_menosigual(subexpressions):
-    'o : asig MENOS IGUAL v'
-    if (types[subexpressions[1]] not in ["int", "float"]): raise SemanticException("Incompatible type")
-    if (types[subexpressions[4]] not in ["int", "float"]): raise SemanticException("Incompatible type")
-    subexpressions[0] = subexpressions[1] + ' ' + subexpressions[2] + subexpressions[3] + ' ' + subexpressions[4]["value"]
+def p_o_menosigual(subexps):
+    'o : v MENOS IGUAL v'
+    if (types[subexps[1]] not in ["int", "float"]): raise SemanticException("Incompatible type")
+    if (types[subexps[4]] not in ["int", "float"]): raise SemanticException("Incompatible type")
+    subexps[0] = subexps[1] + ' ' + subexps[2] + subexps[3] + ' ' + subexps[4]["value"]
         
-def p_o_porigual(subexpressions):
-    'o : asig POR IGUAL v'
-    if (types[subexpressions[1]] not in ["int", "float"]): raise SemanticException("Incompatible type")
-    if (types[subexpressions[4]] not in ["int", "float"]): raise SemanticException("Incompatible type")
-    subexpressions[0] = subexpressions[1] + ' ' + subexpressions[2] + subexpressions[3] + ' ' + subexpressions[4]["value"]
+def p_o_porigual(subexps):
+    'o : v POR IGUAL v'
+    if (types[subexps[1]] not in ["int", "float"]): raise SemanticException("Incompatible type")
+    if (types[subexps[4]] not in ["int", "float"]): raise SemanticException("Incompatible type")
+    subexps[0] = subexps[1] + ' ' + subexps[2] + subexps[3] + ' ' + subexps[4]["value"]
 
-def p_o_divigual(subexpressions):
-    'o : asig DIV IGUAL v'
-    if (types[subexpressions[1]] not in ["int", "float"]): raise SemanticException("Incompatible type")
-    if (types[subexpressions[4]] not in ["int", "float"]): raise SemanticException("Incompatible type")
-    subexpressions[0] = subexpressions[1] + ' ' + subexpressions[2] + subexpressions[3] + ' ' + subexpressions[4]["value"]
+def p_o_divigual(subexps):
+    'o : v DIV IGUAL v'
+    if (types[subexps[1]] not in ["int", "float"]): raise SemanticException("Incompatible type")
+    if (types[subexps[4]] not in ["int", "float"]): raise SemanticException("Incompatible type")
+    subexps[0] = subexps[1] + ' ' + subexps[2] + subexps[3] + ' ' + subexps[4]["value"]
 
-def p_o_masmas(subexpressions):
-    'o : asig SUMA SUMA'
-    if (types[subexpressions[1]] not in ["int", "float"]): raise SemanticException("Incompatible type")
-    subexpressions[0] = subexpressions[1] + subexpressions[2] + subexpressions[3]
+def p_o_masmas(subexps):
+    'o : v SUMA SUMA'
+    if (types[subexps[1]] not in ["int", "float"]): raise SemanticException("Incompatible type")
+    subexps[0] = subexps[1] + subexps[2] + subexps[3]
 
-def p_o_menosmenos(subexpressions):
-    'o : asig MENOS MENOS'
-    if (types[subexpressions[1]] not in ["int", "float"]): raise SemanticException("Incompatible type")
-    subexpressions[0] = subexpressions[1] + subexpressions[2] + subexpressions[3]
+def p_o_menosmenos(subexps):
+    'o : v MENOS MENOS'
+    if (types[subexps[1]] not in ["int", "float"]): raise SemanticException("Incompatible type")
+    subexps[0] = subexps[1] + subexps[2] + subexps[3]
         
-def p_o_masmasv(subexpressions):
-    'o : SUMA SUMA asig'
-    subexpressions[0] = subexpressions[1] + subexpressions[2] + subexpressions[3]
+def p_o_masmasv(subexps):
+    'o : SUMA SUMA v'
+    subexps[0] = subexps[1] + subexps[2] + subexps[3]
 
-def p_o_menosmenosv(subexpressions):
-    'o : MENOS MENOS asig'
-    subexpressions[0] = subexpressions[1] + subexpressions[2] + subexpressions[3]
+def p_o_menosmenosv(subexps):
+    'o : MENOS MENOS v'
+    subexps[0] = subexps[1] + subexps[2] + subexps[3]
 
-def p_o_print(subexpressions):
+def p_o_print(subexps):
     'o : PRINT LPAREN v RPAREN'
-    subexpressions[0] = subexpressions[1] + subexpressions[2] + subexpressions[3] + subexpressions[4]
+    subexps[0] = subexps[1] + subexps[2] + subexps[3] + subexps[4]
 
-def p_bq1(subexpressions):
+def p_bq1(subexps):
 	'bq : s'
-	subexpressions[0] = subexpressions[1] + subexpressions[2] + subexpressions[3]
+	subexps[0] = subexps[1] + subexps[2] + subexps[3]
 	
-def p_bq2(subexpressions):
+def p_bq2(subexps):
 	'bq : LLAVEABIERTA ss LLAVECERRADA'
-	subexpressions[0] = subexpressions[1] + '\n\t' + "\n\t".join(subexpressions[2].split("\n")) + '\n' + subexpressions[3]
+	subexps[0] = subexps[1] + '\n\t' + "\n\t".join(subexps[2].split("\n")) + '\n' + subexps[3]
 
 def p_error(token):
     message = "[Syntax error]"
